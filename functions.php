df74bf74dd033ff01d3aa84d82104dc05c90208b
AdonisBerishaS1X <rubingducks@gmail.com>
2026-03-11 18:57:31 +0100

    mod9

--- functions.php
+++ functions.php
@@ -1,50 +1,47 @@
 <?php
 
 function ds_theme_assets() {
+
+
+ 
     wp_enqueue_style(
         'ds-style',
-         get_stylesheet_uri(),
-         array(),
-         '1.0',
-         'all'
-        );
+        get_stylesheet_uri(),
+        array(),
+        '1.0',
+        'all'
+    );
 
-     wp_enqueue_style(
+
+  
+    wp_enqueue_style(
         'slider-style',
-         get_template_directory_uri() . '/css/slider.css',
-         array(),
-         '1.0',
-         'all'
-        );
+        get_template_directory_uri() . '/css/slider.css',
+        array(),
+        '1.0',
+        'all'
+    );
 
 
- wp_enqueue_style(
+   
+    wp_enqueue_script(
         'ds-script',
-         get_template_directory_uri() . '/js/script.js',
-         array(),
-         '1.0',
-         'all'
-        );
+        get_template_directory_uri() . '/js/custom.js',
+        array('jquery'),
+        '1.0',
+        true
+    );
 
-        if(is_singular()&& comments_open() && get_option('theme_comments')){
-            wp_enqueue_scripts('comment-replay');
-        }
+
+ 
+    if ( is_singular() && comments_open() && get_option('thread_comments') ) {
+        wp_enqueue_script('comment-reply');
+    }
 }
+
+
 
 add_action('wp_enqueue_scripts', 'ds_theme_assets');
-
-function ds_theme_set(){
-    add_theme_support('post-thumbnails');
-
-
-    add_theme_support('post-formats',array('aside','image','video'));
-
-
-    add_theme_support('title-tag');
-}
-
-
-add_action('after_setup_theme','ds_theme_set');
 
 
 
@@ -69,49 +66,49 @@ function ds_add_bootstrap(){
 }
 add_action('wp_enqueue_scripts', 'ds_add_bootstrap');
 
-function themename_widgets(){
-    register_sidebar(
-        array(
-            'name' => __("Primary Sidebar", 'theme_name'),
-            'id' => 'primary-sidebar',
-            'before_widget' => '<aside id="$1%" class="widget %2$s"></aside>'
-            'after_widget' => '</aside>',
-            'before_title' => '<h3 class="widget-title">',
-            'after_title' => '</h3>'
-        )
-        );
-}
+// function themename_widgets(){
+//     register_sidebar(
+//         array(
+//             'name' => __("Primary Sidebar", 'theme_name'),
+//             'id' => 'primary-sidebar',
+//             'before_widget' => '<aside id="$1%" class="widget %2$s">'
+//             'after_widget' => '</aside>',
+//             'before_title' => '<h3 class="widget-title">',
+//             'after_title' => '</h3>'
+//         )
+//         );
+// }
 
 
-add_action('widgets_init', 'themename_widgets_init');
+//add_action('widgets_init', 'themename_widgets_init');
 
-class Foo_Widget extends WP_Widget {
-    public funtion__construct() {
-        parent::__construct(
-            'foo_widget',
-            'foo widget',
-            'A Foo Widget'
-        );
-    }
+// class Foo_Widget extends WP_Widget {
+//     public funtion__construct() {
+//         parent::__construct(
+//             'foo_widget',
+//             'foo widget',
+//             'A Foo Widget'
+//         );
+//     }
 
-    public function widget($args,$instance){
-        echo $args['before_widget'];
-        echo '<p> Hello World </p>';
-        echo '<p> Hello grupi 6A </p>'
-        echo $args['after_widget'];
-    }
+//     public function widget($args,$instance){
+//         echo $args['before_widget'];
+//         echo '<p> Hello World </p>';
+//         echo '<p> Hello grupi 6A </p>'
+//         echo $args['after_widget'];
+//     }
 
-    public function form($instance){
-        echo '<p> No options yet </p>';
-    }
+//     public function form($instance){
+//         echo '<p> No options yet </p>';
+//     }
 
-    public function update($new_instance, $old_instance){
-        return $new_instance;
-    }
-}
+//     public function update($new_instance, $old_instance){
+//         return $new_instance;
+//     }
+// }
 
-function register_foo_widget(){
-    register_widget('Foo_Widget');
-}
-add_action('widgets_init', 'register_foo_widget');
+// function register_foo_widget(){
+//     register_widget('Foo_Widget');
+// }
+// add_action('widgets_init', 'register_foo_widget');
 ?>
---
+++ page-design.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<div class="container my-5">
+    <h1 class="text-center mb-5">Css Design Effects</h1>
+
+    <div class="slider">
+        <div class="slide">
+
+     <img src="<?php echo get_template_directory_uri(); ?>/images/slid1.jpg">
+     <img src="<?php echo get_template_directory_uri(); ?>/images/slid2.jpg">
+     <img src="<?php echo get_template_directory_uri(); ?>/images/slid3.jpg">
+
+</div>
+</div>
+</div>
+
+<div class="row mt-5">
+ <div class="col-md-4">
+<div class="rounded-box">
+Rounded Cornners 
+</div>
+</div>
+
+ <div class="col-md-4">
+<div class="rounded-image-box">
+Border Image
+</div>
+</div>
+
+ <div class="col-md-4">
+<div class="background-box">
+Background Image
+</div>
+</div>
+
+</div>
+
+<?php get_footer(); ?>
